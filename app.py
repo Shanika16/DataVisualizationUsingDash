c22984b94de8d3b33b12b1d852c8021a3408158a
Shanika16 <148519555+Shanika16@users.noreply.github.com>
2024-01-26 23:41:49 -0700

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import dash
+pip install dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Output, Input
